4a468081106c6b8b527743605612fdffb49d07a1
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-06-22 14:43:09 +1000

    button for list view and grid view working

---
+++ src/components/CustomList.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+
+export const CustomList = ({ movie = {}, func, inSearchForm }) => {
+  const { Title, Poster, imdbRating } = movie;
+  return (
+    <Card style={{ width: "18rem" }} className="mt-3">
+      <Card.Img variant="top" src={Poster} />
+      <Card.Body>
+        <Card.Title>{Title}</Card.Title>
+        <Card.Title>Rating: {imdbRating}</Card.Title>
+
+        {inSearchForm ? (
+          <div className="d-flex justify-content-between mt-2">
+            <Button
+              variant="danger"
+              onClick={() => func({ ...movie, mood: "action" })}
+            >
+              Action
+            </Button>
+            <Button
+              variant="success"
+              onClick={() => func({ ...movie, mood: "romantic" })}
+            >
+              Romantic
+            </Button>
+          </div>
+        ) : (
+          <div className="d-grid gap-2">
+            <Button
+              variant="danger"
+              size="lg"
+              onClick={() => func(movie.imdbID)}
+            >
+              Delete Movie
+            </Button>
+          </div>
+        )}
+      </Card.Body>
+    </Card>
+  );
+};
